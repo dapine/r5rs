70fe08854d99e859b79d71afde610a7a1bbf94f2
David Pinheiro <davepinh@gmail.com>
2018-02-04 21:06:33 -0300

    Command line eval

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
 module Main where
 
-import Lib
+import Parser
+import Eval
+import Text.ParserCombinators.Parsec (parse)
+import System.Environment (getArgs)
+
+readExpr :: String -> LispVal
+readExpr input = case parse parseExpr "lisp" input of
+                   Left err -> String $ "No match: " ++ show err
+                   Right val -> val
 
 main :: IO ()
-main = someFunc
+main = getArgs >>= print . eval . readExpr . head
